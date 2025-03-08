d2c84e84f6a2a4587699695830bea4fec893b204
Navyashree0104 <navyashree0104@gmail.com>
2025-03-08 16:42:37 +0530

    commit the initial changes

---
+++ dynamic-page-app/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="page-container">
+      <h1>About Us</h1>
+      <p>
+        At "Flower Bliss", we believe that flowers have the power to "uplift moods, brighten days and spread joy". <br></br>
+        Each petal tells a story, and we bring you closer to nature with beautiful insights about flowers and their meanings. 🌺🌼
+      </p>
+    </div>
+  );
+};
+
+export default About;
